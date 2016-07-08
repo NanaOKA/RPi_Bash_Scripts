db607c93ef18da0401d44d790336169f3724f85a
NanaOKA <nana.abankwa@gmail.com>
2016-07-08 11:25:09 +0100

    Update Setup_ROS.sh

--- Setup_ROS.sh
+++ Setup_ROS.sh
@@ -51,3 +51,9 @@ sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release 
 # Source new installation
 source /opt/ros/indigo/setup.bash
 echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+
+# Setup networking so other laptops can connect to the boat
+# master is the boat
+echo "export ROS_HOSTNAME=192.168.42.1" >> ~/.bashrc
+
+cd ~
